3f9fcbc5d377f2435f27c28380b2c203acf050c0
spartacrypt <spartacrypt@protonmail.com>
2019-10-18 18:44:04 +0100

    Update from code review. Reverted some project files and prevent some tests from running.

--- src/Stratis.Features.FederatedPeg.IntegrationTests/NodeInitialisationTests.cs
+++ src/Stratis.Features.FederatedPeg.IntegrationTests/NodeInitialisationTests.cs
@@ -43,7 +43,7 @@ namespace Stratis.Features.FederatedPeg.IntegrationTests
             this.scriptAndAddresses = FederatedPegTestHelper.GenerateScriptAndAddresses(this.mainNetwork, this.sidechainNetwork, 2, pubKeysByMnemonic);
         }
 
-        [Fact(Skip = "Broken. Re-enable when fixed")]
+        [Fact]
         public void SidechainUserStarts()
         {
             using (SidechainNodeBuilder nodeBuilder = SidechainNodeBuilder.CreateSidechainNodeBuilder(this))
@@ -58,7 +58,7 @@ namespace Stratis.Features.FederatedPeg.IntegrationTests
             }
         }
 
-        [Fact(Skip = "Broken. Re-enable when fixed")]
+        [Fact]
         public void SidechainMinerStarts()
         {
             using (SidechainNodeBuilder nodeBuilder = SidechainNodeBuilder.CreateSidechainNodeBuilder(this))
@@ -109,7 +109,7 @@ namespace Stratis.Features.FederatedPeg.IntegrationTests
             }
         }
 
-        [Fact(Skip = "Broken. Re-enable when fixed")]
+        [Fact]
         public void MainChainGatewayStarts()
         {
             using (SidechainNodeBuilder nodeBuilder = SidechainNodeBuilder.CreateSidechainNodeBuilder(this))

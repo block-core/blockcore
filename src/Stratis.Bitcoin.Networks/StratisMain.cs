7caf79c1c7d8a848eecca6d61c84d7eb1d7b939f
dangershony <dan.gershony@gmail.com>
2020-03-09 20:20:06 +0000

    Fix stratis coldstaking activation dates

--- src/Stratis.Bitcoin.Networks/StratisMain.cs
+++ src/Stratis.Bitcoin.Networks/StratisMain.cs
@@ -102,12 +102,12 @@ namespace Stratis.Bitcoin.Networks
 
                 [StratisBIP9Deployments.Segwit] = new BIP9DeploymentsParameters("Segwit", 1,
                     new DateTime(2019, 10, 1, 0, 0, 0, DateTimeKind.Utc),
-                    new DateTime(2020, 10, 1, 0, 0, 0, DateTimeKind.Utc), 
+                    new DateTime(2020, 10, 1, 0, 0, 0, DateTimeKind.Utc),
                     BIP9DeploymentsParameters.DefaultMainnetThreshold),
 
-                [StratisBIP9Deployments.ColdStaking] = new BIP9DeploymentsParameters("ColdStaking", 2, 
-                    new DateTime(2018, 12, 1, 0, 0, 0, DateTimeKind.Utc),
-                    new DateTime(2019, 12, 1, 0, 0, 0, DateTimeKind.Utc),
+                [StratisBIP9Deployments.ColdStaking] = new BIP9DeploymentsParameters("ColdStaking", 2,
+                    new DateTime(2019, 12, 2, 0, 0, 0, DateTimeKind.Utc),
+                    new DateTime(2020, 12, 2, 0, 0, 0, DateTimeKind.Utc),
                     BIP9DeploymentsParameters.DefaultMainnetThreshold)
             };
 

93eb39771704a47af4e28b6ec3a8677bf6eec69d
dangershony <dan.gershony@gmail.com>
2020-04-15 19:21:01 +0100

    Fix xds and startup

--- src/Blockcore.Networks.Xds/Consensus/XdsProvenBlockHeader.cs
+++ src/Blockcore.Networks.Xds/Consensus/XdsProvenBlockHeader.cs
@@ -13,18 +13,5 @@ namespace Blockcore.Networks.Xds.Consensus
         public XdsProvenBlockHeader(PosBlock block, XdsBlockHeader xdsBlockHeader) : base(block, xdsBlockHeader)
         {
         }
-
-        //public uint256 GetPoWHash()
-        //{
-        //    byte[] serialized;
-
-        //    using (var ms = new MemoryStream())
-        //    {
-        //        this.PosBlockHeader.ReadWriteHashingStream(new BitcoinStream(ms, true));
-        //        serialized = ms.ToArray();
-        //    }
-
-        //    return Sha512T.GetHash(serialized);
-        //}
     }
 }

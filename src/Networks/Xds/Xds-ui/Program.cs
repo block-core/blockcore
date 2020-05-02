b5d03667cbc4f7a00ba2f99f2438b9614af4259a
dangershony <dan.gershony@gmail.com>
2020-05-02 03:09:30 +0100

    Hide console on win os

--- src/Networks/Xds/Xds-ui/Program.cs
+++ src/Networks/Xds/Xds-ui/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Blockcore.Builder;
 using Blockcore.Configuration;
@@ -19,6 +20,15 @@ namespace StratisD
 {
     public class Program
     {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
+
 #pragma warning disable IDE1006 // Naming Styles
 
         public static async Task Main(string[] args)
@@ -44,6 +54,8 @@ namespace StratisD
                 var node = nodeBuilder.Build();
                 node.RunAsync();
 
+                HideConsole();
+
                 var window = new WebWindow(node.Network.CoinTicker + " Node");
                 window.NavigateToUrl(node.NodeService<ApiSettings>().ApiUri.ToString());
                 window.WaitForExit();
@@ -53,6 +65,30 @@ namespace StratisD
             catch (Exception ex)
             {
                 Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.ToString());
+            }
+        }
+
+        public static void HideConsole()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var handle = GetConsoleWindow();
+
+                // Hide
+                ShowWindow(handle, SW_HIDE);
+
+                // Show
+                // ShowWindow(handle, SW_SHOW);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+            }
+            else
+            {
+                // Do nothing
             }
         }
     }

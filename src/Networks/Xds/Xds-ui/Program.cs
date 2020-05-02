f3bf58ddc40aeff4302cb5d13a27a50fc61132a4
dangershony <dan.gershony@gmail.com>
2020-05-02 02:51:51 +0100

    Add webview to xds

---
+++ src/Networks/Xds/Xds-ui/Program.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading.Tasks;
+using Blockcore.Builder;
+using Blockcore.Configuration;
+using Blockcore.Features.Api;
+using Blockcore.Features.BlockStore;
+using Blockcore.Features.ColdStaking;
+using Blockcore.Features.Consensus;
+using Blockcore.Features.Diagnostic;
+using Blockcore.Features.MemoryPool;
+using Blockcore.Features.Miner;
+using Blockcore.Features.RPC;
+using Blockcore.Networks.Xds;
+using Blockcore.Utilities;
+using NBitcoin.Protocol;
+using WebWindows;
+
+namespace StratisD
+{
+    public class Program
+    {
+#pragma warning disable IDE1006 // Naming Styles
+
+        public static async Task Main(string[] args)
+#pragma warning restore IDE1006 // Naming Styles
+        {
+            try
+            {
+                var nodeSettings = new NodeSettings(networksSelector: Networks.Xds,
+                    protocolVersion: ProtocolVersion.PROVEN_HEADER_VERSION,
+                    args: args);
+
+                IFullNodeBuilder nodeBuilder = new FullNodeBuilder()
+                    .UseNodeSettings(nodeSettings)
+                    .UseBlockStore()
+                    .UsePosConsensus()
+                    .UseMempool()
+                    .UseColdStakingWallet()
+                    .AddPowPosMining()
+                    .UseApi()
+                    .AddRPC()
+                    .UseDiagnosticFeature();
+
+                var node = nodeBuilder.Build();
+                node.RunAsync();
+
+                var window = new WebWindow(node.Network.CoinTicker + " Node");
+                window.NavigateToUrl(node.NodeService<ApiSettings>().ApiUri.ToString());
+                window.WaitForExit();
+
+                node.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.ToString());
+            }
+        }
+    }
+}

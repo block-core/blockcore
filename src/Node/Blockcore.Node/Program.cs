e96154e575de794f0a652499e982c28b3f7d99b0
SondreB <sondre@outlook.com>
2020-05-06 23:23:16 +0200

    Add Blockcore.Node project for quick and easy debug of multiple chains (#115)
    - Adds a new project that can launch multiple chains based on startup argument. This allows us to remove the existing node projects, they are no longer needed.

---
+++ src/Node/Blockcore.Node/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Blockcore.Builder;
+using Blockcore.Configuration;
+using Blockcore.Features.SignalR;
+using Blockcore.Features.SignalR.Broadcasters;
+using Blockcore.Features.SignalR.Events;
+using Blockcore.Utilities;
+
+namespace Blockcore.Node
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                string chain = args
+                   .DefaultIfEmpty("--chain=BTC")
+                   .Where(arg => arg.StartsWith("--chain", ignoreCase: true, CultureInfo.InvariantCulture))
+                   .Select(arg => arg.Replace("--chain=", string.Empty, ignoreCase: true, CultureInfo.InvariantCulture))
+                   .FirstOrDefault();
+
+                NodeSettings nodeSettings = NetworkSelector.Create(chain, args);
+                IFullNodeBuilder nodeBuilder = NodeBuilder.Create(chain, nodeSettings);
+
+                if (nodeSettings.EnableSignalR)
+                {
+                    nodeBuilder.AddSignalR(options =>
+                    {
+                        options.EventsToHandle = new[]
+                             {
+                            (IClientEvent) new BlockConnectedClientEvent(),
+                            new TransactionReceivedClientEvent()
+                   };
+
+                        options.ClientEventBroadcasters = new[]
+                             {
+                            (Broadcaster: typeof(StakingBroadcaster), ClientEventBroadcasterSettings: new ClientEventBroadcasterSettings { BroadcastFrequencySeconds = 5 }),
+                            (Broadcaster: typeof(WalletInfoBroadcaster), ClientEventBroadcasterSettings: new ClientEventBroadcasterSettings { BroadcastFrequencySeconds = 5 })
+                   };
+                    });
+                }
+
+                IFullNode node = nodeBuilder.Build();
+
+                if (node != null)
+                    await node.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex);
+            }
+        }
+    }
+}

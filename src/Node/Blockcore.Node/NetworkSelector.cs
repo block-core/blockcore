6242248fb6d329c6f1b0fdf5ee8ab1978dd4ab45
SondreB <sondre@outlook.com>
2020-05-06 18:22:09 +0200

    Add Blockcore.Node project for quick and easy debug of multiple chains
    - Adds a new project that can launch multiple chains based on startup argument. This allows us to remove the existing node projects, they are no longer needed.

---
+++ src/Node/Blockcore.Node/NetworkSelector.cs
@@ -0,0 +1,49 @@
+﻿using Blockcore.Configuration;
+using NBitcoin.Protocol;
+
+namespace Blockcore.Node
+{
+    public static class NetworkSelector
+    {
+        public static NodeSettings Create(string chain, string[] args)
+        {
+            chain = chain.ToUpperInvariant();
+
+            NodeSettings nodeSettings = null;
+
+            switch (chain)
+            {
+                case "BTC":
+                    nodeSettings = new NodeSettings(networksSelector: Blockcore.Networks.Bitcoin.Networks.Bitcoin, args: args);
+                    break;
+                case "CITY":
+                    nodeSettings = new NodeSettings(networksSelector: City.Networks.Networks.City,
+                        protocolVersion: ProtocolVersion.PROVEN_HEADER_VERSION, args: args)
+                    {
+                        MinProtocolVersion = ProtocolVersion.ALT_PROTOCOL_VERSION
+                    };
+                    break;
+                case "STRAT":
+                    nodeSettings = new NodeSettings(networksSelector: Blockcore.Networks.Stratis.Networks.Stratis,
+                        protocolVersion: ProtocolVersion.PROVEN_HEADER_VERSION, args: args);
+                    break;
+                case "X42":
+                    nodeSettings = new NodeSettings(networksSelector: x42.Networks.Networks.x42,
+                        protocolVersion: ProtocolVersion.PROVEN_HEADER_VERSION, args: args)
+                    {
+                        MinProtocolVersion = ProtocolVersion.ALT_PROTOCOL_VERSION
+                    };
+                    break;
+                case "XDS":
+                    nodeSettings = new NodeSettings(networksSelector: Blockcore.Networks.Xds.Networks.Xds,
+                        protocolVersion: ProtocolVersion.PROVEN_HEADER_VERSION, args: args)
+                    {
+                        MinProtocolVersion = ProtocolVersion.ALT_PROTOCOL_VERSION
+                    };
+                    break;
+            }
+
+            return nodeSettings;
+        }
+    }
+}
---
+++ src/Node/Blockcore.Node/NodeBuilder.cs
@@ -0,0 +1,52 @@
+﻿using Blockcore.Builder;
+using Blockcore.Configuration;
+using Blockcore.Features.Api;
+using Blockcore.Features.BlockStore;
+using Blockcore.Features.ColdStaking;
+using Blockcore.Features.Consensus;
+using Blockcore.Features.Diagnostic;
+using Blockcore.Features.MemoryPool;
+using Blockcore.Features.Miner;
+using Blockcore.Features.RPC;
+using Blockcore.Features.Wallet;
+
+namespace Blockcore.Node
+{
+    public static class NodeBuilder
+    {
+        public static IFullNodeBuilder Create(string chain, NodeSettings settings)
+        {
+            chain = chain.ToUpperInvariant();
+
+            IFullNodeBuilder nodeBuilder = CreateBaseBuilder(chain, settings);
+
+            switch (chain)
+            {
+                case "BTC":
+                    nodeBuilder.UsePowConsensus().AddMining().UseWallet();
+                    break;
+                case "CITY":
+                case "STRAT":
+                case "X42":
+                case "XDS":
+                    nodeBuilder.UsePosConsensus().AddPowPosMining().UseColdStakingWallet();
+                    break;
+            }
+
+            return nodeBuilder;
+        }
+
+        private static IFullNodeBuilder CreateBaseBuilder(string chain, NodeSettings settings)
+        {
+            IFullNodeBuilder nodeBuilder = new FullNodeBuilder()
+            .UseNodeSettings(settings)
+            .UseBlockStore()
+            .UseMempool()
+            .UseApi()
+            .AddRPC()
+            .UseDiagnosticFeature();
+
+            return nodeBuilder;
+        }
+    }
+}

f49d87da696810bec734c5f90ab14bf9f1fdb2b0
Hunter <thecryptohunter@protonmail.com>
2020-06-07 11:51:55 +0100

    Add modal component service & creat add node modal

---
+++ src/Features/Blockcore.Features.NodeHost/UI/ModalService.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorModal.Services
+{
+    public class ModalService
+    {
+        public event Action<string, RenderFragment> OnShow;
+        public event Action OnClose;
+
+        public void Show(string title, Type contentType)
+        {
+            if (contentType.BaseType != typeof(ComponentBase))
+            {
+                throw new ArgumentException($"{contentType.FullName} must be a Blazor Component");
+            }
+
+            var content = new RenderFragment(x => { x.OpenComponent(1, contentType); x.CloseComponent(); });
+
+            OnShow?.Invoke(title, content);
+        }
+
+        public void Close()
+        {
+            OnClose?.Invoke();
+        }
+    }
+}

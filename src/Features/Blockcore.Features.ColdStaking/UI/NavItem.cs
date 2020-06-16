8a154fc37cceee734e5047b13b4f3d9fec4fd38f
Hunter <thecryptohunter@protonmail.com>
2020-06-16 12:33:52 +0100

    Add IsVisible to Navigation Item

--- src/Features/Blockcore.Features.ColdStaking/UI/NavItem.cs
+++ src/Features/Blockcore.Features.ColdStaking/UI/NavItem.cs
@@ -1,11 +1,23 @@
 ﻿using Blockcore.Interfaces.UI;
+using Blockcore.Features.Wallet.Interfaces;
 
 namespace Blockcore.Features.Wallet.UI
 {
     public class ColdStakingNavigationItem : INavigationItem
     {
+        private readonly IWalletManager WalletManager;
         public string Name => "Cold Staking";
         public string Navigation => "ColdStaking";
         public string Icon => "oi-pulse";
+        public bool IsVisible => true; //hasWallets();
+        public bool hasWallets() {
+            var walletManager = this.WalletManager as WalletManager;
+            bool hasWallets = false;
+            if (walletManager != null)
+                {
+                    hasWallets = true; //walletManager.ContainsWallets;
+                }
+            return hasWallets;
+        }
     }
 }
--- src/Features/Blockcore.Features.Miner/UI/NavItem.cs
+++ src/Features/Blockcore.Features.Miner/UI/NavItem.cs
@@ -1,11 +1,23 @@
 ﻿using Blockcore.Interfaces.UI;
+using Blockcore.Features.Wallet.Interfaces;
 
 namespace Blockcore.Features.Wallet.UI
 {
     public class StakeNavigationItem : INavigationItem
     {
-        public string Name => "Staking";
+         public string Name => "Staking";
         public string Navigation => "Stake";
-        public string Icon => "oi-bolt";
+        public string Icon => "oi-bolt";     
+        private readonly IWalletManager walletManager;
+        public bool IsVisible => true; //hasWallets();
+        public bool hasWallets() {
+            var walletManager = this.walletManager as WalletManager;
+            bool hasWallets = false;
+            if (walletManager != null)
+                {
+                    hasWallets = true; //walletManager.ContainsWallets;
+                }
+            return hasWallets;
+        }
     }
 }

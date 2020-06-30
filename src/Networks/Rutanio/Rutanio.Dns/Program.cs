6fe3649ce90bb7a7c72d857f51ef0d1a44e7305f
turcol <turcol@gmail.com>
2020-06-30 00:52:53 -0400

    Add Rutanio DNS daemon

---
+++ src/Networks/Rutanio/Rutanio.Dns/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using Blockcore;
+using Blockcore.Builder;
+using Blockcore.Configuration;
+using Blockcore.Features.NodeHost;
+using Blockcore.Features.BlockStore;
+using Blockcore.Features.Consensus;
+using Blockcore.Features.Dns;
+using Blockcore.Features.MemoryPool;
+using Blockcore.Features.Miner;
+using Blockcore.Features.RPC;
+using Blockcore.Features.Wallet;
+using Blockcore.Utilities;
+using NBitcoin.Protocol;
+
+namespace Rutanio.Dns
+{
+    /// <summary>
+    /// Main entry point.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The async entry point for the Rutanio Dns process.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        /// <returns>A task used to await the operation.</returns>
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var nodeSettings = new NodeSettings(networksSelector: Networks.Networks.Rutanio, args: args, agent: "Blockcore-RUTA");
+
+                var dnsSettings = new DnsSettings(nodeSettings);
+
+                if (string.IsNullOrWhiteSpace(dnsSettings.DnsHostName) || string.IsNullOrWhiteSpace(dnsSettings.DnsNameServer) || string.IsNullOrWhiteSpace(dnsSettings.DnsMailBox))
+                    throw new ConfigurationException("When running as a DNS Seed service, the -dnshostname, -dnsnameserver and -dnsmailbox arguments must be specified on the command line.");
+
+                // Run as a full node with DNS or just a DNS service?
+                IFullNode node;
+                if (dnsSettings.DnsFullNode)
+                {
+                    // Build the Dns full node.
+                    node = new FullNodeBuilder()
+                        .UseNodeSettings(nodeSettings)
+                        .UseBlockStore()
+                        .UsePosConsensus()
+                        .UseMempool()
+                        .UseWallet()
+                        .AddPowPosMining()
+                        .UseNodeHost()
+                        .AddRPC()
+                        .UseDns()
+                        .Build();
+                }     
+
+                // Run node.
+                if (node != null)
+                    await node.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex.ToString());
+            }
+        }
+    }
+}

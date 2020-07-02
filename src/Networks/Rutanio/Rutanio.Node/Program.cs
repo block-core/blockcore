ee79a8f080089426c34e0ed0337ee1397731b3ff
turcol <turcol@gmail.com>
2020-07-02 19:35:13 -0400

    Added DNS Service to Rutanio.Node (#175)
    * Added DNS Service to Rutanio.Node
    
    * Re-order left menu
    
    * Remove custom agent

--- src/Networks/Rutanio/Rutanio.Node/Program.cs
+++ src/Networks/Rutanio/Rutanio.Node/Program.cs
@@ -7,6 +7,7 @@ using Blockcore.Features.NodeHost;
 using Blockcore.Features.BlockStore;
 using Blockcore.Features.ColdStaking;
 using Blockcore.Features.Consensus;
+using Blockcore.Features.Dns;
 using Blockcore.Features.Diagnostic;
 using Blockcore.Features.MemoryPool;
 using Blockcore.Features.Miner;
@@ -22,18 +23,21 @@ namespace Rutanio.Daemon
         {
             try
             {
-                var nodeSettings = new NodeSettings(networksSelector: Networks.Networks.Rutanio, args: args, agent: "Blockcore-RUTA");
+                var nodeSettings = new NodeSettings(networksSelector: Networks.Networks.Rutanio, args: args);
 
                 IFullNodeBuilder nodeBuilder = new FullNodeBuilder()
                     .UseNodeSettings(nodeSettings)
                     .UseBlockStore()
-                    .UseMempool()
-                    .UseNodeHost()
-                    .AddRPC()
-                    .UseDiagnosticFeature()
                     .UsePosConsensus()
+                    .UseMempool()
                     .AddPowPosMining()
                     .UseColdStakingWallet();
+                    .UseDiagnosticFeature()
+                    .UseNodeHost()
+                    .AddRPC();
+
+                // Build the Dns full node if enabled
+                UseDnsFullNode(nodeBuilder, nodeSettings);
 
                 IFullNode node = nodeBuilder.Build();
 
@@ -43,6 +47,18 @@ namespace Rutanio.Daemon
             catch (Exception ex)
             {
                 Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex);
+            }
+        }
+        static void UseDnsFullNode(IFullNodeBuilder nodeBuilder, NodeSettings nodeSettings)
+        {
+            if (nodeSettings.ConfigReader.GetOrDefault("dnsfullnode", false, nodeSettings.Logger))
+            {
+                var dnsSettings = new DnsSettings(nodeSettings);
+
+                if (string.IsNullOrWhiteSpace(dnsSettings.DnsHostName) || string.IsNullOrWhiteSpace(dnsSettings.DnsNameServer) || string.IsNullOrWhiteSpace(dnsSettings.DnsMailBox))
+                    throw new ConfigurationException("When running as a DNS Seed service, the -dnshostname, -dnsnameserver and -dnsmailbox arguments must be specified on the command line.");
+
+                nodeBuilder.UseDns();
             }
         }
     }

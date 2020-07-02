57acad01252b3687ed07bd36a59606a428943d00
turcol <turcol@gmail.com>
2020-07-02 19:32:14 -0400

    Adding OpenExO Network (EXOS) (#169)
    * Adding OpenExo Networks
    
    * Added OpenExo.Node
    
    * Added OpenExo to Blockcore.Node
    
    * Network replaced by Block Height in the dashboard
    
    * Password type changed in sending form
    
    * Update BIP9 params for CVS
    
    * Add OpenExo DNS daemon
    
    * Updated launchSettings OpenExo Dns
    
    * Updated OpenExo BIP9Deployments file
    
    * Added dns service to OpenExo.Dns
    
    * Added launchSettings.json to OpenExo.Node
    
    * Removed OpenExo dns daemon
    
    * Added dns flag validation to OpenExo.Node
    
    * Removed OpenExo agent from Blockcore.Node
    
    Co-authored-by: checho1989 <cheverdolaga@hotmail.com>

---
+++ src/Networks/OpenExo/OpenExo/Networks/Rules/OpenExoBugFixPosFutureDriftRule.cs
@@ -0,0 +1,44 @@
+﻿using Blockcore.Features.Consensus.Rules.CommonRules;
+
+namespace OpenExo.Networks.Rules
+{
+    /// <summary>
+    /// A rule that will verify the block time drift is according to the PoS consensus rules for the <see cref="OpenExoMain"/> network (and its test networks).
+    /// New networks must use the <see cref="PosFutureDriftRule"/>.
+    /// </summary>
+    public class OpenExoBugFixPosFutureDriftRule : PosFutureDriftRule
+    {
+        /// <summary>Drifting Bug Fix, hardfork on Wed May 16,2018 00:00:00 GMT.</summary>
+        public const long DriftingBugFixTimestamp = 1526428800;
+
+        /// <summary>Old future drift in seconds before the hardfork.</summary>
+        private const int BugFutureDriftSeconds = 128 * 60 * 60;
+
+        /// <summary>
+        /// Gets future drift for the provided timestamp.
+        /// </summary>
+        /// <remarks>
+        /// Future drift is maximal allowed block's timestamp difference over adjusted time.
+        /// If this difference is greater block won't be accepted.
+        /// </remarks>
+        /// <param name="time">UNIX timestamp.</param>
+        /// <returns>Value of the future drift.</returns>
+        public override long GetFutureDrift(long time)
+        {
+            return IsDriftReduced(time) ? FutureDriftSeconds : BugFutureDriftSeconds;
+        }
+
+        /// <summary>
+        /// Checks whether the future drift should be reduced after provided timestamp.
+        /// </summary>
+        /// <param name="time">UNIX timestamp.</param>
+        /// <returns><c>true</c> if for this timestamp future drift should be reduced, <c>false</c> otherwise.</returns>
+        private static bool IsDriftReduced(long time)
+        {
+            // This is a specific Stratis bug fix where the blockchain drifted 24 hour ahead as the protocol allowed that.
+            // The protocol was fixed but historical blocks are still effected.
+            return time > DriftingBugFixTimestamp;
+        }
+
+    }
+}

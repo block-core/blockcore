2d022e1995eb772468cdb77448c86b712bef727e
checho1989 <cheverdolaga@hotmail.com>
2020-07-01 20:20:55 -0400

    Added dns flag validation to OpenExo.Node

--- src/Networks/OpenExo/OpenExo.Node/Program.cs
+++ src/Networks/OpenExo/OpenExo.Node/Program.cs
@@ -7,6 +7,7 @@ using Blockcore.Features.NodeHost;
 using Blockcore.Features.BlockStore;
 using Blockcore.Features.ColdStaking;
 using Blockcore.Features.Consensus;
+using Blockcore.Features.Dns;
 using Blockcore.Features.Diagnostic;
 using Blockcore.Features.MemoryPool;
 using Blockcore.Features.Miner;
@@ -22,18 +23,21 @@ namespace OpenExo.Node
         {
             try
             {
-                var nodeSettings = new NodeSettings(networksSelector: Networks.Networks.OpenExo, args: args, agent: "Blockcore-EXOS");
+                var nodeSettings = new NodeSettings(networksSelector: Networks.Networks.OpenExo, args: args);
 
                 IFullNodeBuilder nodeBuilder = new FullNodeBuilder()
                     .UseNodeSettings(nodeSettings)
                     .UseBlockStore()
-                    .UseMempool()
-                    .UseNodeHost()
-                    .AddRPC()
-                    .UseDiagnosticFeature()
                     .UsePosConsensus()
+                    .UseMempool()
                     .AddPowPosMining()
-                    .UseColdStakingWallet();
+                    .UseColdStakingWallet()
+                    .UseDiagnosticFeature()                    
+                    .UseNodeHost()
+                    .AddRPC();
+
+                // Build DNS FullNode if enable
+                UseDnsFullNode(nodeBuilder, nodeSettings);
 
                 IFullNode node = nodeBuilder.Build();
 
@@ -43,6 +47,19 @@ namespace OpenExo.Node
             catch (Exception ex)
             {
                 Console.WriteLine("There was a problem initializing the node. Details: '{0}'", ex);
+            }
+        }
+
+        static void UseDnsFullNode(IFullNodeBuilder nodeBuilder, NodeSettings nodeSettings)
+        {
+            if (nodeSettings.ConfigReader.GetOrDefault("dnsfullnode", false, nodeSettings.Logger))
+            {
+                var dnsSettings = new DnsSettings(nodeSettings);
+
+                if (string.IsNullOrWhiteSpace(dnsSettings.DnsHostName) || string.IsNullOrWhiteSpace(dnsSettings.DnsNameServer) || string.IsNullOrWhiteSpace(dnsSettings.DnsMailBox))
+                    throw new ConfigurationException("When running as a DNS Seed service, the -dnshostname, -dnsnameserver and -dnsmailbox arguments must be specified on the command line.");
+
+                nodeBuilder.UseDns();
             }
         }
     }

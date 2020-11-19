1c0ad770dd7ae8985924b750a0a574c172729887
SondreB <sondre@outlook.com>
2020-11-19 01:47:49 +0100

    Fix the whitelist to verify against remote IP

--- src/Blockcore/P2P/Peer/NetworkPeerServer.cs
+++ src/Blockcore/P2P/Peer/NetworkPeerServer.cs
@@ -237,7 +237,7 @@ namespace Blockcore.P2P.Peer
 
             var clientLocalEndPoint = tcpClient.Client.LocalEndPoint as IPEndPoint;
 
-            bool endpointCanBeWhiteListed = this.connectionManagerSettings.Whitelist.Any(x => x.MatchIpOnly(clientLocalEndPoint));
+            bool endpointCanBeWhiteListed = this.connectionManagerSettings.Whitelist.Any(x => x.MatchIpOnly(clientRemoteEndPoint));
 
             if (endpointCanBeWhiteListed)
             {

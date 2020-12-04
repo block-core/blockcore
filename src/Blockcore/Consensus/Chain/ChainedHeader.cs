261c8c948d61464507fc830ce23a0119f9b78b44
dangershony <dan.gershony@gmail.com>
2020-12-04 14:47:45 +0000

    Fix more warnings

--- src/Blockcore/Consensus/Chain/ChainedHeader.cs
+++ src/Blockcore/Consensus/Chain/ChainedHeader.cs
@@ -231,8 +231,10 @@ namespace Blockcore.Consensus.Chain
             this.CalculateChainWork();
 
             if (header is PosBlockHeader posBlockHeader)
+            {
                 if (posBlockHeader.ProvenBlockHeader != null)
                     this.ProvenBlockHeader = posBlockHeader.ProvenBlockHeader;
+            }
         }
 
         public ChainedHeader(BlockHeader header, uint256 headerHash, int height) : this(header, headerHash)
@@ -305,7 +307,7 @@ namespace Blockcore.Consensus.Chain
 
                 // Exponentially larger steps back, plus the genesis block.
                 int height = Math.Max(pindex.Height - nStep, 0);
-                pindex = GetAncestor(height);
+                pindex = this.GetAncestor(height);
 
                 if (blockHashes.Count > 10)
                     nStep *= 2;
@@ -379,7 +381,7 @@ namespace Blockcore.Consensus.Chain
         /// to verify the correct chained block header has been found.</remarks>
         public ChainedHeader FindAncestorOrSelf(ChainedHeader chainedHeader)
         {
-            ChainedHeader found = GetAncestor(chainedHeader.Height);
+            ChainedHeader found = this.GetAncestor(chainedHeader.Height);
             if ((found != null) && (found.HashBlock == chainedHeader.HashBlock))
                 return found;
 
@@ -414,7 +416,7 @@ namespace Blockcore.Consensus.Chain
         /// <returns>The target proof of work.</returns>
         public Target GetNextWorkRequired(Network network)
         {
-            return GetNextWorkRequired(network.Consensus);
+            return this.GetNextWorkRequired(network.Consensus);
         }
 
         /// <summary>
@@ -427,7 +429,7 @@ namespace Blockcore.Consensus.Chain
             BlockHeader dummy = consensus.ConsensusFactory.CreateBlockHeader();
             dummy.HashPrevBlock = this.HashBlock;
             dummy.BlockTime = DateTimeOffset.UtcNow;
-            return GetNextWorkRequired(dummy, consensus);
+            return this.GetNextWorkRequired(dummy, consensus);
         }
 
         /// <summary>
@@ -438,7 +440,7 @@ namespace Blockcore.Consensus.Chain
         /// <returns>The target proof of work.</returns>
         public Target GetNextWorkRequired(BlockHeader block, Network network)
         {
-            return GetNextWorkRequired(block, network.Consensus);
+            return this.GetNextWorkRequired(block, network.Consensus);
         }
 
         /// <summary>
@@ -459,7 +461,7 @@ namespace Blockcore.Consensus.Chain
         /// <returns>The target proof of work.</returns>
         public Target GetWorkRequired(Network network)
         {
-            return GetWorkRequired(network.Consensus);
+            return this.GetWorkRequired(network.Consensus);
         }
 
         /// <summary>
@@ -507,7 +509,7 @@ namespace Blockcore.Consensus.Chain
             // Go back by what we want to be 14 days worth of blocks.
             long pastHeight = lastBlock.Height - (difficultyAdjustmentInterval - 1);
 
-            ChainedHeader firstChainedHeader = GetAncestor((int)pastHeight);
+            ChainedHeader firstChainedHeader = this.GetAncestor((int)pastHeight);
             if (firstChainedHeader == null)
                 throw new NotSupportedException("Can only calculate work of a full chain");
 
@@ -574,7 +576,7 @@ namespace Blockcore.Consensus.Chain
                 return BlockStake.Validate(network, this);
 
             bool genesisCorrect = (this.Height != 0) || this.HashBlock == network.GetGenesis().GetHash();
-            return genesisCorrect && Validate(network.Consensus);
+            return genesisCorrect && this.Validate(network.Consensus);
         }
 
         /// <summary>
@@ -593,7 +595,7 @@ namespace Blockcore.Consensus.Chain
             bool heightCorrect = (this.Height == 0) || (this.Height == this.Previous.Height + 1);
             bool hashPrevCorrect = (this.Height == 0) || (this.Header.HashPrevBlock == this.Previous.HashBlock);
             bool hashCorrect = this.HashBlock == this.Header.GetHash();
-            bool workCorrect = CheckProofOfWorkAndTarget(consensus);
+            bool workCorrect = this.CheckProofOfWorkAndTarget(consensus);
 
             return heightCorrect && hashPrevCorrect && hashCorrect && workCorrect;
         }
@@ -605,7 +607,7 @@ namespace Blockcore.Consensus.Chain
         /// <returns>Whether proof of work is valid.</returns>
         public bool CheckProofOfWorkAndTarget(Network network)
         {
-            return CheckProofOfWorkAndTarget(network.Consensus);
+            return this.CheckProofOfWorkAndTarget(network.Consensus);
         }
 
         /// <summary>
@@ -615,7 +617,7 @@ namespace Blockcore.Consensus.Chain
         /// <returns>Whether proof of work is valid.</returns>
         public bool CheckProofOfWorkAndTarget(IConsensus consensus)
         {
-            return (this.Height == 0) || (this.Header.CheckProofOfWork() && (this.Header.Bits == GetWorkRequired(consensus)));
+            return (this.Height == 0) || (this.Header.CheckProofOfWork() && (this.Header.Bits == this.GetWorkRequired(consensus)));
         }
 
         /// <summary>
@@ -733,7 +735,7 @@ namespace Blockcore.Consensus.Chain
             // but the following expression was taken from bitcoin core. There it was tested in simulations
             // and performed well.
             // Skip steps are exponential - Using skip, max 110 steps to go back up to 2^18 blocks.
-            return (height & 1) != 0 ? InvertLowestOne(InvertLowestOne(height - 1)) + 1 : InvertLowestOne(height);
+            return (height & 1) != 0 ? this.InvertLowestOne(this.InvertLowestOne(height - 1)) + 1 : this.InvertLowestOne(height);
         }
 
         /// <summary>
--- src/Blockcore/Interfaces/UI/INavigationItem.cs
+++ src/Blockcore/Interfaces/UI/INavigationItem.cs
@@ -7,10 +7,13 @@ namespace Blockcore.Interfaces.UI
     public interface INavigationItem
     {
         public string Name { get; }
+
         public string Navigation { get; }
+
         public string Icon { get; }
-        public bool IsVisible {get; }
-        public int NavOrder {get; }
-        
+
+        public bool IsVisible { get; }
+
+        public int NavOrder { get; }
     }
 }

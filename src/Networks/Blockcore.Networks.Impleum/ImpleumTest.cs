a538fd73a9603fc3dda39d0c7b0865c782a3bdaa
shtah <1c@ua.fm>
2020-12-14 02:19:51 +0200

    Upate MinTxFee TestNet

--- src/Networks/Blockcore.Networks.Impleum/ImpleumTest.cs
+++ src/Networks/Blockcore.Networks.Impleum/ImpleumTest.cs
@@ -22,7 +22,7 @@ namespace Blockcore.Networks.Impleum
             this.DefaultPort = ImpleumSetup.Test.DefaultPort;
             this.DefaultRPCPort = ImpleumSetup.Test.DefaultRPCPort;
             this.DefaultAPIPort = ImpleumSetup.Test.DefaultAPIPort;
-            this.MinTxFee = 1000;
+            this.MinTxFee = 10000;
 
             var consensusFactory = new PosConsensusFactory();
 

5114f10cf590aa5cdb30b52fd2ac46a9da7ba6bf
Dan Gershony <dan.gershony@gmail.com>
2021-01-30 13:33:32 +0000

    Subscribe to blocks and peers events on the ui (#267)
    * Subscribe to blocks and peers events on the ui
    
    * Add disposable to ui event subscription
    
    * Add notifications for wallet sent coins and refresh UI on such notifications

---
+++ src/Features/Blockcore.Features.Wallet/Events/TransactionSpent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blockcore.Consensus.TransactionInfo;
+using Blockcore.EventBus;
+using NBitcoin;
+
+namespace Blockcore.Features.Wallet.Events
+{
+    /// <summary>
+    /// Event that is executed when a transactions output is spent in the wallet.
+    /// </summary>
+    /// <seealso cref="Blockcore.EventBus.EventBase" />
+    public class TransactionSpent : EventBase
+    {
+        public Transaction SpentTransaction { get; }
+
+        public OutPoint SpentOutPoint { get; }
+
+        public TransactionSpent(Transaction spentTransaction, OutPoint spentOutPoint)
+        {
+            this.SpentTransaction = spentTransaction;
+            this.SpentOutPoint = spentOutPoint;
+        }
+    }
+}

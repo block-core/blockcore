d4e2aad8d61fd4ef1781e243ce01b29c1d6abe11
Dennis <41244965+DennisAMenace@users.noreply.github.com>
2021-03-01 13:25:56 -0700

    Checkpoint

--- src/Networks/Blockcore.Networks.x42/x42Setup.cs
+++ src/Networks/Blockcore.Networks.x42/x42Setup.cs
@@ -62,7 +62,7 @@ namespace Blockcore.Networks.x42
                 { 500000, new CheckpointInfo(new uint256("0x7f9a88ebb32f47090ec37a110c5a05c1162a604dfbfb69c8d492e771cdb63289"), new uint256("0x19db6890c5c934e883bc99eb197509b0a81f19faeefcf49fd7fa6dab83644bfb")) },
                 { 800000, new CheckpointInfo(new uint256("0x981083b047ecf8157a8b2dc24e17ca8cfad01b4e2dabc314df97f3b64fdf37f5"), new uint256("0xf3f0a821801b32c73a7c4f42416ddad3f74b732bd517c968a9b93a33d3684e0b")) },
                 { 1000000, new CheckpointInfo(new uint256("0x1f5900bc62ddc11a383f8602d744fab1afa1e1969f0bf7f6b1b161476739a35e"), new uint256("0xca5fcf25a5561ebc91c5624b7c5ff697060f8a613e53e7a7e90abac925324e39")) },
-                { 1185640, new CheckpointInfo(new uint256("0xba1f6c0a2840a00e89e0486dbd34efbfd23f7ed187dd8e0632a8058452ed5ff5"), new uint256("0xecf8c3c040d97f0a22fb821bfab9442779ecd206cf3e0938b32cf20c05f5bd6f")) }
+                { 1200000, new CheckpointInfo(new uint256("0x183585d27df96ee7acbfa55354a6a87827155fe24b0647791e9e81e45c9ec409"), new uint256("0x891e78eb5ede58c0a909c6af8581c46263a3b0bb94a5f69a0165c8492071e8c6")) }
             }
         };
 

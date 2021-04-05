24234c4ac914030b179fc95b803366b099bd3c44
Peter Williams <pewilliams@programmed.com.au>
2021-04-05 11:58:26 +1000

    SERF Init

--- src/Node/Blockcore.Node/NodeBuilder.cs
+++ src/Node/Blockcore.Node/NodeBuilder.cs
@@ -51,6 +51,7 @@ namespace Blockcore.Node
                 case "XLR":
                 case "IMPLX":
                 case "HOME":
+                case "SERF":
                     nodeBuilder.UsePosConsensus().AddPowPosMining().UseColdStakingWallet();
                     break;
             }

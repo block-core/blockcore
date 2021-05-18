0efcc9dec081a973f0e4a1c3a7d29b26e39b6f00
Sondre <sondre@outlook.com>
2021-05-18 13:58:02 +0200

    Cache the deployment flags to improve startup
    - The deployment flags are cached when calculated as new blocks come in.
    - The deployment flags are read from cache at startup, as they cannot have changed since shutdown.
    - This gives a very big improvement on node startup.
    - #337

--- src/Features/Blockcore.Features.Consensus/Rules/CommonRules/SetActivationDeploymentsPartialValidationRule.cs
+++ src/Features/Blockcore.Features.Consensus/Rules/CommonRules/SetActivationDeploymentsPartialValidationRule.cs
@@ -1,17 +1,28 @@
 ﻿using System.Threading.Tasks;
 using Blockcore.Consensus.Rules;
+using Blockcore.Utilities.Store;
 
 namespace Blockcore.Features.Consensus.Rules.CommonRules
 {
     /// <summary>Set the <see cref="RuleContext.Flags"/> property that defines what deployments have been activated.</summary>
     public class SetActivationDeploymentsPartialValidationRule : PartialValidationConsensusRule
     {
+        private readonly IKeyValueRepository keyValueRepository;
+
+        public SetActivationDeploymentsPartialValidationRule(IKeyValueRepository keyValueRepository)
+        {
+            this.keyValueRepository = keyValueRepository;
+        }
+
         /// <inheritdoc />
         /// <exception cref="ConsensusErrors.InvalidPrevTip">The tip is invalid because a reorg has been detected.</exception>
         public override Task RunAsync(RuleContext context)
         {
             // Calculate the consensus flags and check they are valid.
             context.Flags = this.Parent.NodeDeployments.GetFlags(context.ValidationContext.ChainedHeaderToValidate);
+
+            // Update the cache of Flags when we retrieve it.
+            this.keyValueRepository.SaveValueJson("deploymentflags", context.Flags);
 
             return Task.CompletedTask;
         }

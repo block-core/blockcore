1cd66799b9b5ec0bda9186bfad9d1d4f62586c40
rodendron <rodendron@xrhodium.org>
2021-09-17 20:07:34 +0100

    Extracting root account interface so that it can be substituted
    Extracting root account interface so that it can be substituted by multisig wallet classes

---
+++ src/Features/Blockcore.Features.Wallet/Interfaces/IAccountRoot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Blockcore.Features.Wallet.Database;
+using Blockcore.Networks;
+using NBitcoin;
+
+namespace Blockcore.Features.Wallet.Interfaces
+{
+    public interface IAccountRoot
+    {
+        ICollection<IHdAccount> Accounts { get; set; }
+        int? CoinType { get; set; }
+        uint256 LastBlockSyncedHash { get; set; }
+        int? LastBlockSyncedHeight { get; set; }
+
+        IHdAccount AddNewAccount(ExtPubKey accountExtPubKey, int accountIndex, Network network, DateTimeOffset accountCreationTime);
+        IHdAccount AddNewAccount(string password, string encryptedSeed, byte[] chainCode, Network network, DateTimeOffset accountCreationTime, int? accountIndex = null, string accountName = null);
+        IHdAccount CreateAccount(string password, string encryptedSeed, byte[] chainCode, Network network, DateTimeOffset accountCreationTime, int newAccountIndex, string newAccountName = null);
+        IHdAccount GetAccountByName(string accountName);
+        IHdAccount GetFirstUnusedAccount(IWalletStore walletStore);
+    }
+}

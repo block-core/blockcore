39459b3a4c692cf6f3e0ec221c1f269d5ab3edc1
rodendron <rodendron@xrhodium.org>
2021-09-17 20:09:32 +0100

    Adding multisig scheme class

---
+++ src/Features/Blockcore.Features.Wallet/Types/MultisigScheme.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Blockcore.Features.Wallet.Types
+{
+    public class MultisigScheme
+    {
+        /// <summary>
+        /// How many signatures will be suffient to move the funds.
+        /// </summary>
+        [JsonProperty(PropertyName = "threashold")]
+        public int Threashold { get; set; }
+
+        /// <summary>
+        /// Cosigner extended pubkeys. Intentionally not including any xPriv at this stage as such model is simplest to start with.
+        /// </summary>
+        [JsonProperty(PropertyName = "xPubs")]
+        public string[] XPubs { get; set; }
+    }
+}

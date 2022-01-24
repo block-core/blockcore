6059e56f5f13129c28c875f1a0fd059b9035ebc8
mohsen maftooh <maftooh.m@outlook.com>
2022-01-24 18:56:38 +0330

    Finish building mine page

---
+++ src/Features/Blockcore.Features.Wallet/Exceptions/MiningNotification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blockcore.Features.Wallet.Exceptions
+{
+    public static class MiningNotification
+    {
+        public static EventHandler<bool> MiningChanged;
+
+        private static bool _miningchanged;
+        public static bool OnChangedStatus
+        {
+            get => _miningchanged;
+            set
+            {
+                if (_miningchanged != value)
+                {
+                    _miningchanged = value;
+                    MiningChanged?.Invoke(typeof(MiningNotification), _miningchanged);
+                }
+            }
+        }
+
+    }
+}

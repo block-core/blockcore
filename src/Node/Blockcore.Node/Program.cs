16b155d0f8de9524163911540357dcc66f4d104e
dimitrios.savva1 <dimsavva@gmail.com>
2022-02-09 14:20:49 +0200

    Database Upgrade feature

--- src/Node/Blockcore.Node/Program.cs
+++ src/Node/Blockcore.Node/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Blockcore.Builder;
 using Blockcore.Configuration;
 using Blockcore.Utilities;
-
+ 
 namespace Blockcore.Node
 {
     public class Program
@@ -24,6 +26,50 @@ namespace Blockcore.Node
                 {
                     chain = "BTC";
                 }
+
+                string version = args
+                                   .Where(arg => arg.StartsWith("--upgradedbonversion", ignoreCase: true, CultureInfo.InvariantCulture))
+                                   .Select(arg => arg.Replace("--upgradedbonversion=", string.Empty, ignoreCase: true, CultureInfo.InvariantCulture))
+                                   .FirstOrDefault();
+
+                if (version != null && version != "") {
+
+
+                    var appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+                    var x42MainFolder = appDataFolder + "\\Blockcore\\x42\\x42Main";
+
+                    VersionFileData versionFileData = new VersionFileData() { Version = version, Upgraded = false };
+                    string fileName = x42MainFolder + "\\upgrade.json";
+
+                    if (!File.Exists(fileName))
+                    {
+
+                        File.WriteAllText(fileName, JsonSerializer.Serialize(versionFileData));
+                    }
+                    else {
+
+                        versionFileData = JsonSerializer.Deserialize<VersionFileData>(File.ReadAllText(fileName));
+                  
+                    }
+
+                    if (versionFileData.Version == version && versionFileData.Upgraded == false)
+                    {
+                        var txDbData = appDataFolder + "\\Blockcore\\x42\\x42Main\\txdb";
+                        if (Directory.Exists(txDbData))
+                        {
+                            Directory.Delete(txDbData, true);
+
+                        }
+                        versionFileData.Upgraded = true;
+                        File.WriteAllText(fileName, JsonSerializer.Serialize(versionFileData));
+
+                    }
+
+                }
+
+
+
 
                 NodeSettings nodeSettings = NetworkSelector.Create(chain, args);
                 IFullNodeBuilder nodeBuilder = NodeBuilder.Create(chain, nodeSettings);

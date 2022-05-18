dd61feda35661c6265480fd998d766bd115091e3
Dan Gershony <dan.gershony@gmail.com>
2022-05-18 18:35:04 +0100

    Add sign/verify schnorr using bouncy castle (#404)

--- src/Blockcore/NBitcoin/Utils.cs
+++ src/Blockcore/NBitcoin/Utils.cs
@@ -366,7 +366,7 @@ namespace NBitcoin
             ms.Write(bytes, 0, bytes.Length);
         }
 
-        internal static Array BigIntegerToBytes(BigInteger b, int numBytes)
+        internal static byte[] BigIntegerToBytes(BigInteger b, int numBytes)
         {
             if (b == null)
             {

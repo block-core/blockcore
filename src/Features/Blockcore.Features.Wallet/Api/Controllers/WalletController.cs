4aea4aa20e182919d421cafe131f833f3241bd27
Dimitrios Savva <dimsavva@gmail.com>
2022-08-22 09:24:42 +0200

    Added endpoint to derive a public key from an address

--- src/Features/Blockcore.Features.Wallet/Api/Controllers/WalletController.cs
+++ src/Features/Blockcore.Features.Wallet/Api/Controllers/WalletController.cs
@@ -964,6 +964,41 @@ namespace Blockcore.Features.Wallet.Api.Controllers
         }
 
         /// <summary>
+        /// Get the public key for a specified address, wallet and account
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="walletName"></param>
+        /// <param name="accountName"></param>
+        /// <returns></returns>
+
+
+        [Route("publicKey")]
+        [HttpGet]
+        public IActionResult GetPublicKey(string address, string walletName, string accountName)
+        {
+            int index = 1;
+            int change = 0;
+
+            var keyPath = new KeyPath($"{change}/{index}");
+
+            Types.Wallet wallet = this.walletManager.GetWallet(walletName);
+            HdAddress hdAddress = wallet.GetAddress(address, account => account.Name.Equals(accountName));
+
+            try
+            {
+                return Ok(hdAddress.Pubkey.ToHex());
+
+            }
+            catch (Exception e)
+            {
+
+                this.logger.LogError(e.Message);
+                return ErrorHelpers.BuildErrorResponse(HttpStatusCode.BadRequest, e.Message, e.ToString());
+            }
+
+        }
+
+        /// <summary>
         /// Lists all the files found in the default wallet folder.
         /// </summary>
         /// <returns>A JSON object containing the wallet folder path and

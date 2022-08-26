405629be433e79180ccb14da4df38880e8b91e4c
Dan Gershony <dan.gershony@gmail.com>
2022-08-26 16:22:53 +0100

    Add error handling for rpc trx broad cast (#413)
    * Add error handling for rpc trx broad cast
    
    * Fix failing test

--- src/Tests/Blockcore.IntegrationTests/RPC/RPCTests.cs
+++ src/Tests/Blockcore.IntegrationTests/RPC/RPCTests.cs
@@ -5,6 +5,7 @@ using Blockcore.Connection;
 using Blockcore.Consensus.BlockInfo;
 using Blockcore.Consensus.TransactionInfo;
 using Blockcore.Features.RPC;
+using Blockcore.Features.RPC.Exceptions;
 using Blockcore.Features.Wallet;
 using Blockcore.Features.Wallet.Interfaces;
 using Blockcore.IntegrationTests.Common.EnvironmentMockUpHelpers;
@@ -226,8 +227,12 @@ namespace Blockcore.IntegrationTests.RPC
         public void SendRawTransaction()
         {
             var tx = new Transaction();
+            tx.Inputs.Add(new TxIn());
             tx.Outputs.Add(new TxOut(Money.Coins(1.0m), new Key()));
-            this.rpcTestFixture.RpcClient.SendRawTransaction(tx);
+            var ex = Assert.Throws<RPCException>(() => this.rpcTestFixture.RpcClient.SendRawTransaction(tx));
+
+            // just check that the method reached the rpc we don't need to check if a trx is valid.
+            Assert.Equal(ex.Message, "invalid coinbase size");
         }
 
         /// <summary>

f916b23c5ea6fb4387864ca85d6fe86b3eb09de8
dangershony <dan.gershony@gmail.com>
2023-02-27 09:25:11 +0000

    Add missing attribute

--- src/Blockcore/P2P/Protocol/Payloads/NotFoundPayload.cs
+++ src/Blockcore/P2P/Protocol/Payloads/NotFoundPayload.cs
@@ -9,6 +9,7 @@ namespace Blockcore.P2P.Protocol.Payloads
     /// <summary>
     /// A getdata message for an asked hash is not found by the remote peer.
     /// </summary>
+    [Payload("notfound")]
     public class NotFoundPayload : Payload, IEnumerable<InventoryVector>
     {
         private List<InventoryVector> inventory = new List<InventoryVector>();
